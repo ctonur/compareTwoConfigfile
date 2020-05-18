ee9fedc79bdd3d8173b676f67e8f8ad44cc42d9a
ctonur <35571438+ctonur@users.noreply.github.com>
2020-05-18 03:42:09 +0300

    Delete gkecluster.tf

--- case/gkecluster.tf
+++
@@ -1,6 +0,0 @@
-resource "google_container_cluster" "gke-cluster" {
-  name               = "case-study"
-  network            = "default"
-  location           = "europe-west3-c"
-  initial_node_count = 3
-}
